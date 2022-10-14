d1e192c82c6b5e47e562b4a1ced20e254c79152d
martinv <martinv@educ.college-montreal.qc.ca>
2022-10-14 09:54:19 -0400

    suicide

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
-#Vicente felipe martin 405
-chaine = input("Veuillez ecrire la phrase dont vous cherchez le nombre de motshub ")
-def word_count():
-    print(len(chaine.split(" ")))
-word_count()
+
+import random
+restart = True
+
+
+nb_essais = 1
+
+Choix_1 = int(input("Choisi la borne minimale du jeux "))
+Choix_2 = int(input("Choisi la borne maximale du jeux "))
+nombre = random.randint(Choix_1, Choix_2)
+
+
+
+while restart:
+
+
+    user_that_guess = int(input("Ecriver un nombre entre la borne minimale et maximale"))
+    if user_that_guess > nombre:
+        print("Le chiffre est plus bas")
+        nb_essais +=1
+
+    elif user_that_guess < nombre:
+        print("Le chiffre est plus gros")
+        nb_essais +=1
+
+
+    elif user_that_guess == nombre:
+        print("Bravo vous l'avez trouvé ")
+        print("Vous avez réussi en : " , nb_essais ,"essais" )
+
+
+        finish = int(input("Voules vous recommencer? apuyez 1 pour OUI et 2 pour NON "))
+
+        if finish == 1:
+            nombre = random.randint(Choix_1, Choix_2)
+
+        elif finish == 2:
+            restart = False
+
+
+
